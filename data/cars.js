10f41378fd91abe0ee9b0bf4a37676fd1076997d
Dylan Floyd <floyd800@gmail.com>
2021-09-29 14:38:00 -0700

    initial setup

---
+++ data/cars.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    id: 1,
+    make: 'Tesla',
+    model: 'Model 3',
+    releaseYear: 2017,
+    stillProduced: true,
+    energyType: 'electric'
+  },
+  {
+    id: 2,
+    make: 'Ford',
+    model: 'Mustang',
+    releaseYear: 1964,
+    stillProduced: true,
+    energyType: 'gas'
+  },
+  {
+    id: 3,
+    make: 'Mazda',
+    model: 'Mazda3',
+    releaseYear: 2003,
+    stillProduced: true,
+    energyType: 'gas'
+  },
+  {
+    id: 4,
+    make: 'Mazda',
+    model: 'Miata',
+    releaseYear: 1989,
+    stillProduced: true,
+    energyType: 'gas'
+  },
+  {
+    id: 5,
+    make: 'Toyota',
+    model: 'Prius',
+    releaseYear: 1997,
+    stillProduced: true,
+    energyType: 'hybrid'
+  }
+];
