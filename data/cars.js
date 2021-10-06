1368da211f4f60f3c9222b424ddd98a79d44fed6
Dylan Floyd <floyd800@gmail.com>
2021-10-06 13:26:26 -0700

    add images to car data and api

--- data/cars.js
+++ data/cars.js
@@ -5,7 +5,8 @@ module.exports = [
     releaseYear: 2017,
     stillProduced: true,
     energyType: 'electric',
-    category: 'Compact Car'
+    category: 'Compact Car',
+    img: 'https://tesla-cdn.thron.com/delivery/public/image/tesla/c82315a6-ac99-464a-a753-c26bc0fb647d/bvlatuR/std/1200x628/lhd-model-3-social'
   },
   {
     make: 'Ford',
@@ -13,7 +14,8 @@ module.exports = [
     releaseYear: 1964,
     stillProduced: true,
     energyType: 'gas',
-    category: 'Sports Car'
+    category: 'Sports Car',
+    img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2021-ford-mustang-mach-1-109-1592231891.jpg?crop=0.691xw:0.779xh;0.208xw,0.0625xh&resize=640:*'
   },
   {
     make: 'Mazda',
@@ -21,7 +23,8 @@ module.exports = [
     releaseYear: 2003,
     stillProduced: true,
     energyType: 'gas',
-    category: 'Compact Car'
+    category: 'Compact Car',
+    img: 'https://i.insider.com/5fb42fb21c741f0019aca441'
   },
   {
     make: 'Mazda',
@@ -29,7 +32,8 @@ module.exports = [
     releaseYear: 1989,
     stillProduced: true,
     energyType: 'gas',
-    category: 'Roadster'
+    category: 'Roadster',
+    img: 'https://cdn.jdpower.com/JDPA_2020%20Mazda%20MX-5%20Miata%20RF%20Grand%20Touring%20Polymetal%20Gray%20Front%20View.jpg'
   },
   {
     make: 'Toyota',
@@ -37,6 +41,7 @@ module.exports = [
     releaseYear: 1997,
     stillProduced: true,
     energyType: 'hybrid',
-    category: 'Compact Car'
+    category: 'Compact Car',
+    img: 'https://media.ed.edmunds-media.com/toyota/prius/2019/oem/2019_toyota_prius_4dr-hatchback_l-eco_fq_oem_1_1280.jpg'
   }
 ];
